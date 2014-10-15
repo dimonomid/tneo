a2869d84ead35b59a3d591a733f56ebe53ddfe71
Dmitry Frank <dimon.frank@gmail.com>
2014-10-16 02:09:33 +0400

    refactor in queue example project organization

---
+++ examples/queue/arch/example_arch.h
@@ -0,0 +1,4 @@
+
+//-- Include common pic32 header for all examples
+#include "../../arch/pic32/example_arch.h"
+
---
+++ examples/queue/arch/pic32_arch.c
@@ -0,0 +1,4 @@
+
+//-- Include common pic32 source for all examples
+#include "../../arch/pic32/pic32_arch.c"
+
