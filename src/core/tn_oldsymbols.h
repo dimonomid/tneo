644a2b3d509f3a9e888bd64a006bde98adfac9cd
Dmitry Frank <dimon.frank@gmail.com>
2014-12-11 00:12:30 +0300

    FIX: old TNKernel events API mode was wrong: tn_event_clear/iclear take inverted pattern

--- src/core/tn_oldsymbols.h
+++ src/core/tn_oldsymbols.h
@@ -349,15 +349,15 @@ typedef struct TN_EventGrp    TN_EVENT;
 #define  TN_EVENT_ATTR_CLR             TN_EVENTGRP_ATTR_CLR
 
 #define  tn_event_create(ev, attr, pattern)  \
-         tn_eventgrp_create_wattr(ev, (enum TN_EGrpAttr)(attr), pattern)
+         tn_eventgrp_create_wattr((ev), (enum TN_EGrpAttr)(attr), (pattern))
 #define  tn_event_delete               tn_eventgrp_delete
 #define  tn_event_wait                 tn_eventgrp_wait
 #define  tn_event_wait_polling         tn_eventgrp_wait_polling
 #define  tn_event_iwait                tn_eventgrp_iwait_polling
-#define  tn_event_set(ev, pattern)     tn_eventgrp_modify (ev, TN_EVENTGRP_OP_SET, pattern)
-#define  tn_event_iset(ev, pattern)    tn_eventgrp_imodify(ev, TN_EVENTGRP_OP_SET, pattern)
-#define  tn_event_clear(ev, pattern)   tn_eventgrp_modify (ev, TN_EVENTGRP_OP_CLEAR, pattern)
-#define  tn_event_iclear(ev, pattern)  tn_eventgrp_imodify(ev, TN_EVENTGRP_OP_CLEAR, pattern)
+#define  tn_event_set(ev, pattern)     tn_eventgrp_modify ((ev), TN_EVENTGRP_OP_SET, (pattern))
+#define  tn_event_iset(ev, pattern)    tn_eventgrp_imodify((ev), TN_EVENTGRP_OP_SET, (pattern))
+#define  tn_event_clear(ev, pattern)   tn_eventgrp_modify ((ev), TN_EVENTGRP_OP_CLEAR, (~(pattern)))
+#define  tn_event_iclear(ev, pattern)  tn_eventgrp_imodify((ev), TN_EVENTGRP_OP_CLEAR, (~(pattern)))
 
 #else
 //-- no compatibility with event API
